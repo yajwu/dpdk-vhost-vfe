00b630a1db2db1907c40a0858e03954fb0e16e54
Yajun Wu <yajunw@nvidia.com>
2022-07-05 05:42:19 +0300

    prep new vfe cli

--- configs/build_dpdk.sh
+++ configs/build_dpdk.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #unset PYTHONPATH
-meson build -Dexamples=vdpa --buildtype=debug
+#meson build -Dexamples=vdpa --buildtype=debug
+meson build -Dexamples=vdpa,vhost_blk -Dc_args='-DRTE_LIBRTE_VDPA_DEBUG' --debug
 ninja -C build
 #cp cmd.vdpa ./build/examples/cmd -f
 
--- configs/conf.sh
+++ configs/conf.sh
@@ -13,14 +13,14 @@ export testtype=net
 export netpf=0000:3b:00.2
 export blkpf=0000:3b:00.3
 export bf2ip=gen-l-vrt-316-bf
-export p0ip=1.1.1.5
+export p0ip=1.1.2.5
 export p0eth=enp59s0f0
 
 export vmname=gen-l-vrt-439-141-CentOS-7.4
 export vmxml=gen-l-vrt-439-141-CentOS-7.4.xml
 export vmip=gen-l-vrt-439-141
 export vmeth=eth1
-export vmethip=1.1.1.20
+export vmethip=1.1.2.20
 export devmac="e4:11:c6:d3:45:f3"
 
 export peer=gen-l-vrt-440
@@ -32,7 +32,7 @@ if   [ `hostname` == "gen-l-vrt-440" ]; then
 	#export vmname=gen-l-vrt-440-161-RH-8.2
 	#export vmxml=gen-l-vrt-440-161-RH-8.2.xml
 	#export vmip=gen-l-vrt-440-161
-	export p0ip=1.1.1.5
+	export p0ip=1.1.2.6
 	export bf2ip=gen-l-vrt-317-bf
 	export peer=gen-l-vrt-439
 fi
