8f4c5a9c7a757b878596db2f46cf667c05697cd0
Yajun Wu <yajunw@nvidia.com>
2022-09-19 15:01:18 +0800

    devtoolset-8 for centos

--- configs/build_dpdk.sh
+++ configs/build_dpdk.sh
@@ -2,7 +2,9 @@
 
 #unset PYTHONPATH
 #meson build -Dexamples=vdpa --buildtype=debug
+
+source scl_source enable devtoolset-8
+
 meson build -Dexamples=vdpa,vhost_blk -Dc_args='-DRTE_LIBRTE_VDPA_DEBUG' --debug
 ninja -C build
-#cp cmd.vdpa ./build/examples/cmd -f
 
--- tools/_show_ver.sh
+++ tools/_show_ver.sh
@@ -13,13 +13,13 @@ sshpass -p centos ssh root@$bf2ip "cd /root/upstream/nvmx; git rlog -1"
 echo -e "\nvirtio-net-controller: "
 sshpass -p centos ssh root@$bf2ip "cd /root/upstream/virtio-net-controller; git rlog -1"
 echo -e "\ndpdk: "
-pushd sw/dpdk 1>/dev/null && git rlog -1 && popd 1>/dev/null
+git -C /images/testvfe/sw/dpdk log --no-merges --pretty=format:'%h%d (%ci) %s  <%an>' -1 
 echo -e "\nqemu: "
-pushd sw/qemu 1>/dev/null && git rlog -1 && popd 1>/dev/null
+git -C /images/testvfe/sw/qemu log --no-merges --pretty=format:'%h%d (%ci) %s  <%an>' -1
 
 
 echo -e "\n == files =="
-ls -l sw/dpdk/build/app/dpdk-vfe-vdpa
+ls -l /images/testvfe/sw/dpdk/build/app/dpdk-vfe-vdpa
 sshpass -p centos ssh root@$bf2ip ls -l /usr/lib64/libsnap.so.0.0.0
 sshpass -p centos ssh root@$bf2ip ls -l /usr/bin/mlnx_snap_emu
 sshpass -p centos ssh root@$bf2ip ls -l /usr/sbin/virtio_net_controller
