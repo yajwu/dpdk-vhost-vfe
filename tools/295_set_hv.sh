651a00f208f5650e88b7390e8b3eb635be0fafd8
Yajun Wu <yajunw@nvidia.com>
2022-10-31 15:09:51 +0800

    295 socket name

--- tools/295_set_hv.sh
+++ tools/295_set_hv.sh
@@ -2,9 +2,9 @@
 
 
 sudo ifconfig tmfifo_net0 192.168.100.1
-sudo ifconfig ens4f0np0  1.1.5.5/24
+sudo ifconfig ens4f0np0  1.1.7.6/24
 
-sudo arp -s 1.1.5.10 00:00:00:00:33:00
+sudo arp -s 1.1.7.20 00:00:00:00:33:00
 sudo arp -s 1.1.5.11 00:00:00:00:33:01
 sudo arp -s 1.1.5.12 00:00:00:00:33:02
 sudo arp -s 1.1.5.13 00:00:00:00:33:03
